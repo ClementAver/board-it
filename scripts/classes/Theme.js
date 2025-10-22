c42141d664349234d2f11eafdc5d8cb27d29a3d1
clement.aver <clement.aver@yahoo.fr>
2025-10-22 08:06:57 +0200

    [feat][refa] Animates theme switch button and refactors colour settings inside theme.css.

--- scripts/classes/Theme.js
+++ scripts/classes/Theme.js
@@ -2,11 +2,13 @@ class Theme {
   _currentTheme = null;
   _prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
   _switchBtn = document.querySelector("[data-theme-switch-btn]");
+  _switchHandle = document.querySelector("[data-theme-switch-handle]");
 
-  constructor({ currentTheme, prefersDark, switchBtn } = {}) {
+  constructor({ currentTheme, prefersDark, switchBtn, switchHandle } = {}) {
     this.currentTheme = currentTheme ?? this._currentTheme;
     this.prefersDark = prefersDark ?? this._prefersDark;
     this.switchBtn = switchBtn ?? this._switchBtn;
+    this.switchHandle = switchHandle ?? this._switchHandle;
 
     this.switchBtn.addEventListener("click", () => {
       this.currentTheme =
@@ -32,6 +34,10 @@ class Theme {
     return this._switchBtn;
   }
 
+  get switchHandle() {
+    return this._switchHandle;
+  }
+
   set currentTheme(currentTheme) {
     this._currentTheme = currentTheme;
   }
@@ -42,6 +48,10 @@ class Theme {
 
   set switchBtn(switchBtn) {
     this._switchBtn = switchBtn;
+  }
+
+  set switchHandle(switchHandle) {
+    this._switchHandle = switchHandle;
   }
 
   initTheme() {
@@ -56,12 +66,27 @@ class Theme {
   }
 
   setTheme(theme) {
-    console.log(theme);
-
     const pressed = theme === "dark" ? "true" : "false";
     document.documentElement.setAttribute("data-theme", theme);
     localStorage.setItem("theme", theme);
     this.switchBtn.setAttribute("aria-pressed", pressed);
+    this.updateIcon(theme);
+  }
+
+  // TODO : XSS -> to be refactored.
+  updateIcon(theme) {
+    const pressed = theme === "dark" ? true : false;
+    if (pressed) {
+      this.switchHandle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="lucide lucide-moon-icon lucide-moon" viewBox="0 0 24 24">
+  <path d="M21 12a9 9 0 1 1-9-9v1a6 6 0 0 0 8 8z"/>
+</svg>`;
+    } else {
+      this.switchHandle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="lucide lucide-sun-icon lucide-sun" viewBox="0 0 24 24">
+  <circle cx="12" cy="12" r="4"/>
+  <path d="M12 2v2M12 20v2M5 5l1 1M18 18l1 1M2 12h2M20 12h2M6 18l-1 1M19 5l-1 1"/>
+</svg>
+`;
+    }
   }
 }
 
