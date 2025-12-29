b2f11cba858accbe35b841ae25ca394c513c09b4
clement.aver <clement.aver@yahoo.fr>
2025-12-29 20:51:42 +0100

    [refa] thumbnails now uses the light DOM in order to get styled from the outside.

--- scripts/classes/Thumbnail.js
+++ scripts/classes/Thumbnail.js
@@ -6,7 +6,6 @@ class Thumbnail extends HTMLElement {
   _image = document.createElement("img");
   _caption = document.createElement("figcaption");
   _loader = document.createElement("div");
-  _shadow = this.attachShadow({ mode: "open" });
 
   constructor() {
     super();
@@ -31,14 +30,15 @@ class Thumbnail extends HTMLElement {
     this.setCaption();
     this._figure.append(this._caption);
 
-    const style = document.createElement("style");
-    style.textContent = this.style;
+    this.classList.add("thumbnail");
 
     this._article.appendChild(this._figure);
     this._article.appendChild(this._loader);
-    this._shadow.appendChild(this._article);
-    this._shadow.appendChild(style);
+
+    this.appendChild(this._article);
   }
+
+  static observedAttributes = ["alt", "caption", "src"];
 
   attributeChangedCallback(name, oldValue, newValue) {
     if (newValue === oldValue) return;
@@ -69,94 +69,6 @@ class Thumbnail extends HTMLElement {
   setSource(source) {
     this._image.src = source ?? this.getAttribute("src") ?? "";
   }
-
-  style = `
-  article {
-    background-color: var(--secondary);
-    border-radius: var(--spacing-sm);
-    height: 100%;
-    max-height: 150px;
-    max-width: 150px;
-    overflow: hidden;
-    width: 100%;
-  }
-
-  figure {
-    display: none;
-    margin: 0;
-    height: 100%;
-    width: 100%;
-    grid-template-areas: "main"
-  }
-
-  img {
-    grid-area: main;
-    height: 100%;
-    object-fit: cover;
-    width: 100%;
-  }
-
-  figcaption {
-    background-color: var(--accent-beta);
-    border-radius: var(--spacing-sm);
-    box-shadow: var(--shadow-std);  
-    box-sizing: border-box;
-    color: var(--primary);
-    grid-area: main;
-    margin: auto 1rem 1rem auto;
-    max-width: calc(100% - 2rem);
-    overflow: hidden;
-    padding-inline: var(--spacing-sm);
-    text-overflow: ellipsis;
-    white-space: nowrap;
-  }
-
-  :has(.loader) {
-    position: relative;
-  }
-
-  .loader {
-    background-color: var(--secondary);
-    position: absolute;
-    inset: 0;
-    background: linear-gradient(to right, var(--secondary), var(--secondary-darker), var(--secondary));
-    background-size: 200% 100%;
-    animation: loading-background 2s infinite;
-  }
-
-  .loader.error {
-    animation: unset;
-    background: unset;
-  }
-  .loader.error::after {
-    background-color: var(--invalid);
-    border-radius: var(--spacing-sm);
-    box-shadow: var(--shadow-std);  
-    box-sizing: border-box;
-    color: var(--text-swath);
-    content: "Erreur";
-    display: inline-block;
-    height: fit-content;
-    left: 50%;
-    max-width: calc(100% - 2rem);
-    overflow: hidden;
-    padding-inline: var(--spacing-sm);
-    position: absolute;
-    text-overflow: ellipsis;
-    top: 50%;
-    transform: translateX(-50%) translateY(-50%);
-    white-space: nowrap;
-  }
-
-  @keyframes loading-background {
-    0% {
-      background-position: 0% 200%;
-    }
-    100% {
-      background-position: -200% 0%;
-    }
-  }
-  `;
 }
 
 customElements.define("aeee-thumbnail", Thumbnail);
