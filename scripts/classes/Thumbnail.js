736976162b4e98ac0174c4f6a1f5ea058d406b9c
clement.aver <clement.aver@yahoo.fr>
2026-01-01 21:30:05 +0100

    [feat] Making a modification on a property or an attribute on the Thumbnail component will now also modify the other.

--- scripts/classes/Thumbnail.js
+++ scripts/classes/Thumbnail.js
@@ -1,81 +1,155 @@
 import handleError from "../utilities/handleError.js";
 
-// TODO: https://web.dev/articles/custom-elements-best-practices?hl=fr
+// TODO : Add a rounded way for js.
 class Thumbnail extends HTMLElement {
+  _alternate = "";
   _article = document.createElement("article");
+  _caption = "";
+  _figcaption = null;
   _figure = document.createElement("figure");
   _image = document.createElement("img");
-  _caption = null;
   _loader = document.createElement("div");
+  _source = "";
 
-  constructor() {
+  constructor({ src, alt, caption } = {}) {
     super();
 
-    this.classList.add("thumbnail");
+    this.alternate = alt ?? this.getAttribute("alt") ?? this.alterante;
+    this.caption = caption ?? this.getAttribute("caption") ?? this.caption;
+    this.source = src ?? this.getAttribute("src") ?? this.source;
 
-    this._loader.classList.add("loader");
+    this._loader.classList.add("loader"); // TODO : replace by an attribute.
 
-    this._image.onload = () => {
-      this._figure.style.display = "grid";
-      this._loader.style.display = "none";
+    this.image.loading = "lazy";
+    this.image.onload = () => {
+      this.loader.style.display = "none";
+      this.loader.classList.remove("error");
     };
-    this._image.onerror = (error) => {
-      this._loader.classList.add("error");
+    this.image.onerror = (error) => {
+      this.loader.classList.add("error");
       handleError({
         text: `Une Erreur est survenue lors du chargement d'une image.`,
         error,
       });
     };
-    this.setSource();
-    this.setAlternateText();
-    this._figure.appendChild(this._image);
 
-    const caption = this.getAttribute("caption");
-    if (this.getAttribute("rounded")) {
-      this._article.classList.add("rounded-full");
-    } else if (caption) {
-      this._caption = document.createElement("figcaption");
-      this.setCaption(caption);
-      this._figure.appendChild(this._caption);
-    }
+    this.figure.appendChild(this.image);
+    this.article.appendChild(this.figure);
+    this.article.appendChild(this.loader);
+    this.appendChild(this.article);
 
-    this._article.appendChild(this._figure);
-    this._article.appendChild(this._loader);
-    this.appendChild(this._article);
+    console.dir(this);
   }
 
-  static observedAttributes = ["alt", "caption", "src"];
+  get alternate() {
+    return this._alternate;
+  }
+
+  get article() {
+    return this._article;
+  }
+
+  get caption() {
+    return this._caption;
+  }
+
+  get figcaption() {
+    return this._figcaption;
+  }
+
+  get figure() {
+    return this._figure;
+  }
+
+  get image() {
+    return this._image;
+  }
+
+  get loader() {
+    return this._loader;
+  }
+
+  get source() {
+    return this._source;
+  }
+
+  set alternate(alternate) {
+    if (this.getAttribute("alt") !== alternate) {
+      this.setAttribute("alt", alternate);
+      return;
+    }
+
+    this._alternate = alternate;
+    this.image.alt = alternate;
+  }
+
+  set article(article) {
+    this._article = article;
+  }
+
+  set caption(caption) {
+    if (this.getAttribute("caption") !== caption) {
+      this.setAttribute("caption", caption);
+      return;
+    }
+
+    this._caption = caption;
+
+    if (!this.figcaption) {
+      this.figcaption = document.createElement("figcaption");
+      this.figure.appendChild(this.figcaption);
+    }
+
+    this.figcaption.textContent = caption;
+  }
+
+  set figcaption(figcaption) {
+    this._figcaption = figcaption;
+  }
+
+  set figure(figure) {
+    this._figure = figure;
+  }
+
+  set image(image) {
+    this._image = image;
+  }
+
+  set loader(loader) {
+    this._loader = loader;
+  }
+
+  set source(source) {
+    if (this.getAttribute("src") !== source) {
+      this.setAttribute("src", source);
+      return;
+    }
+
+    this.loader.style.display = "block";
+
+    this._source = source;
+    this.image.src = source;
+  }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    if (newValue === oldValue) return;
+    if (oldValue === newValue) return;
 
     switch (name) {
       case "alt":
-        this.setAlternateText(newValue);
+        this.alternate = newValue;
         break;
       case "caption":
-        this.setCaption(newValue);
+        this.caption = newValue;
         break;
       case "src":
-        this.setSource(newValue);
+        this.source = newValue;
         break;
       default:
         break;
     }
   }
-  HTMLImage;
-  setAlternateText(alternateText) {
-    this._image.alt = alternateText ?? this.getAttribute("alt") ?? "";
-  }
 
-  setCaption(caption) {
-    if (this._caption)
-      this._caption.textContent = caption ?? this.getAttribute("caption") ?? "";
-  }
-
-  setSource(source) {
-    this._image.src = source ?? this.getAttribute("src") ?? "";
-  }
+  static observedAttributes = ["alt", "caption", "src"];
 }
 
 customElements.define("aeee-thumbnail", Thumbnail);
