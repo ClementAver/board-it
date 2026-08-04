c4b3924e47981b9619d834948d02798f17705eff
clement.aver <clement.aver@yahoo.fr>
2026-08-04 20:40:42 +0200

    [feat] Upgrades DragSorter.

--- scripts/classes/DragSorter.js
+++ scripts/classes/DragSorter.js
@@ -6,18 +6,27 @@ import { throttle } from "../utilities/timing.js";
 export default class DragSorter extends HTMLElement {
   constructor() {
     super();
-    this.addEventListener("drag", throttle(this.drag, 250));
+    this.addEventListener("drag", throttle(this.drag.bind(this), 250));
+    this.addEventListener("dragstart", (event) => {
+      event.target.closest('[draggable="true"]')?.classList.add("is-dragged");
+    });
+    this.addEventListener("dragover", (event) => {
+      event.preventDefault();
+    });
+    this.addEventListener("dragend", (event) => {
+      event.target
+        .closest('[draggable="true"]')
+        ?.classList.remove("is-dragged");
+    });
   }
 
-  // TODO: Investigate on cursor on 'drag' events.
-
   drag(event) {
-    event.stopImmediatePropagation();
     const dragged = event.target.closest('[draggable="true"]');
     const underneath = document
       .elementFromPoint(event.clientX, event.clientY)
       .closest('[draggable="true"]');
     if (!underneath) return;
+    
     const { x } = getCenterCoords(underneath);
     const where = event.clientX >= x ? "after" : "before";
 
@@ -25,10 +34,37 @@ export default class DragSorter extends HTMLElement {
       dragged.parentElement.parentElement ===
       underneath.parentElement.parentElement
     ) {
-      event.dataTransfer.dropEffect = "move";
+      if (event.dataTransfer) event.dataTransfer.dropEffect = "move";
       insertSibling(dragged, underneath, where);
     } else {
-      event.dataTransfer.dropEffect = "none";
+      if (event.dataTransfer) event.dataTransfer.dropEffect = "none";
+    }
+  }
+
+  drag(event) {
+    const dragged = event.target.closest('[draggable="true"]');
+    const underneath = document
+      .elementFromPoint(event.clientX, event.clientY)
+      .closest('[draggable="true"]');
+    if (!underneath) return;
+
+    const { x } = getCenterCoords(underneath);
+    const where = event.clientX >= x ? "after" : "before";
+
+    if (
+      // same generation
+      dragged.parentElement.parentElement ===
+      underneath.parentElement.parentElement
+    ) {
+      if (event.dataTransfer) event.dataTransfer.dropEffect = "move";
+      insertSibling(dragged, underneath, where);
+    } else if (
+      // one generation gap
+      dragged.parentElement.parentElement === underneath.parentElement
+    ) {
+      underneath.appendChild(dragged);
+    } else {
+      if (event.dataTransfer) event.dataTransfer.dropEffect = "none";
     }
   }
 }
