ccb7bbc2a6056bbd1071e92a87d3f93ce8b189f2
clement.aver <clement.aver@yahoo.fr>
2026-08-11 21:12:42 +0200

    [feat] Works on the Board custom component class.

--- scripts/classes/Board.js
+++ scripts/classes/Board.js
@@ -1,20 +1,57 @@
+import Svg from "./Svg.js";
+import insertSibling from "../utilities/insertSibling.js";
+
 export default class Board extends HTMLElement {
   #title = "";
+  #titleElement = null;
+  #edit = null;
+  #delete = null;
 
   constructor({ title } = {}) {
     super();
 
-    this.#setupDOM(({ title } = {}));
+    this.#title = title ?? this.#title;
   }
 
-  #setupDOM({ title } = {}) {
-    this.#title = title ?? this.querySelector(["data-title"]) ?? this.#title;
+  connectedCallback() {
+    this.setupDOM();
+  }
+
+  setupDOM() {
+    const header =
+      this.querySelector("header") ?? document.createElement("header");
+
+    this.titleElement =
+      header.querySelector("[data-title]") ?? document.createElement("p");
+    this.title =
+      this.title || this.dataset.title || this.titleElement.textContent.trim();
+    if (!header.contains(this.titleElement))
+      header.insertBefore(this.titleElement, header.firstElementChild);
+
+    this.edit =
+      header.querySelector('aeee-svg[data-href*="#square-pen"]') ??
+      new Svg({ href: "../assets/icons/sprites.svg#square-pen" });
+    if (!header.contains(this.edit))
+      insertSibling(this.edit, this.titleElement, "after");
+
+    this.delete =
+      header.querySelector('aeee-svg[data-href*="#trash-2"]') ??
+      new Svg({ href: "../assets/icons/sprites.svg#trash-2" });
+    if (!header.contains(this.delete))
+      insertSibling(this.delete, this.edit, "after");
+
+    if (!this.contains(header))
+      this.insertBefore(header, this.firstElementChild);
   }
 
   #setupEvents() {}
 
   get title() {
     return this.#title;
+  }
+
+  get titleElement() {
+    return this.#titleElement;
   }
 
   set title(title) {
@@ -24,8 +61,11 @@ export default class Board extends HTMLElement {
     }
 
     this.#title = title;
-    let t = this.querySelector(".title");
-    if (t) t.textContent = title;
+    if (this.titleElement) this.titleElement.textContent = title;
+  }
+
+  set titleElement(titleElement) {
+    this.#titleElement = titleElement;
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
