8be67f63440aba7ac908af96ad56b9eb3429e6fb
clement.aver <clement.aver@yahoo.fr>
2026-08-13 22:30:44 +0200

    [feat] Works on the Board custom component class.

--- scripts/classes/Board.js
+++ scripts/classes/Board.js
@@ -28,17 +28,27 @@ export default class Board extends HTMLElement {
     if (!header.contains(this.titleElement))
       header.insertBefore(this.titleElement, header.firstElementChild);
 
-    this.edit =
-      header.querySelector('aeee-svg[data-href*="#square-pen"]') ??
+    this.editBtn =
+      header.querySelector('button:has(>[data-href*="#square-pen"])') ??
+      document.createElement("button");
+    this.editSvg =
+      header.querySelector('[data-href*="#square-pen"]') ??
       new Svg({ href: "../assets/icons/sprites.svg#square-pen" });
-    if (!header.contains(this.edit))
-      insertSibling(this.edit, this.titleElement, "after");
+    if (!this.editBtn.contains(this.editSvg))
+      this.editBtn.appendChild(this.editSvg);
+    if (!header.contains(this.editBtn))
+      insertSibling(this.editBtn, this.titleElement, "after");
 
-    this.delete =
-      header.querySelector('aeee-svg[data-href*="#trash-2"]') ??
+    this.deleteBtn =
+      header.querySelector('button:has(>[data-href*="#trash-2"])') ??
+      document.createElement("button");
+    this.deleteSvg =
+      header.querySelector('[data-href*="#trash-2"]') ??
       new Svg({ href: "../assets/icons/sprites.svg#trash-2" });
-    if (!header.contains(this.delete))
-      insertSibling(this.delete, this.edit, "after");
+    if (!this.deleteBtn.contains(this.deleteSvg))
+      this.deleteBtn.appendChild(this.deleteSvg);
+    if (!header.contains(this.deleteBtn))
+      insertSibling(this.deleteBtn, this.editBtn, "after");
 
     if (!this.contains(header))
       this.insertBefore(header, this.firstElementChild);
