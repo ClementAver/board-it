00d9bbf38fd314e22103e3558a2967a2f364db8e
Clément AVER <clement.aver@raccourci.fr>
2026-09-17 11:32:54 +0200

    [wip] Works on the API class.

--- scripts/classes/API.js
+++ scripts/classes/API.js
@@ -1,34 +1,59 @@
-class API {
-  #headers = null;
+import { debounce, throtle } from "../utilities/timing.js";
+
+export class API {
   #origin = "";
+  #registered = new Map();
 
-  constructor({ headers, origin } = {}) {
-    this.#headers = this.#headersGuard(headers) ?? this.#headers;
+  constructor({ origin } = {}) {
     this.#origin = origin ?? this.#origin;
-  }
-
-  get headers() {
-    return this.#headers;
   }
 
   get origin() {
     return this.#origin;
   }
 
-  set headers(headers) {
-   this.#headers = this.#headersGuard(headers) ?? this.#headers;
+  get registered() {
+    return this.#registered;
   }
 
   set origin(origin) {
     this.#origin = origin;
   }
 
-  #headersGuard(headers) {
-    if (headers && headers instanceof Headers) {
-      return headers;
-    } else {
-      console.error(`expected type Headers, found ${typeof headers}`);
-      return false;
+  async register(
+    key,
+    { method, pathname, headers, signal, timming, delay, options } = {},
+  ) {
+    if (this.registered.has(key)) {
+      throw new Error(`function already registered for key: ${key}`);
     }
+
+    let fn = async ({ body } = {}) => {
+      const response = await fetch(pathname, {
+        method,
+        headers,
+        body,
+        signal,
+      });
+      if (!response.ok) {
+        throw new Error(`response status: ${response.status}`);
+      }
+      return response;
+    };
+
+    if (timming === "debounce") fn = debounce(fn, delay, options);
+    else if (timming === "throttle") fn = throtle(fn, delay);
+
+    this.registered.set(symbol(key), fn);
+  }
+
+  async call(key, { body } = {}) {
+    if (!this.registered.has(key)) {
+      throw new Error(`no function registered for key: ${key}`);
+    }
+    let fn = this.registered.get(key);
+    return await fn(body);
   }
 }
+
+export default new API();
--- scripts/utilities/timing.js
+++ scripts/utilities/timing.js
@@ -17,28 +17,23 @@
  * @param { boolean | undefined } options.leading
  * @param { boolean | undefined } options.trailing
  */
-
 export function debounce(
   callback,
   delay = 0,
   { leading = false, trailing = true } = {},
 ) {
   let timeoutID;
-
   return (args) => {
     return new Promise(async (resolve, reject) => {
       try {
         if (!timeoutID) {
           if (leading) callback.apply(null, [args]);
         }
-
         if (timeoutID) clearTimeout(timeoutID);
-
         timeoutID = setTimeout(() => {
           timeoutID = false;
           if (trailing) callback.apply(null, [args]);
         }, delay);
-
         resolve(true);
       } catch (error) {
         reject(error);
@@ -56,28 +51,23 @@ export function debounce(
 export function throttle(callback, delay = 0) {
   let lastCall;
   let timeoutID;
-
   return (args) => {
     const now = Date.now();
     if (timeoutID) clearTimeout(timeoutID);
-
     return new Promise(async (resolve, reject) => {
       try {
         if (!lastCall || now >= lastCall + delay) {
           lastCall = now;
-
           await callback.apply(null, [args]);
         } else {
           timeoutID = setTimeout(
             async () => {
               lastCall = Date.now();
-
               await callback.apply(null, [args]);
             },
             Math.min(delay - (now - lastCall), delay),
           );
         }
-
         resolve(true);
       } catch (error) {
         reject(error);
